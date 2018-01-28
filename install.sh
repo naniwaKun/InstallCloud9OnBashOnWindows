1462f775605498fd3506fe1a747b45f30306026a
naniwaKun <radio.naniwa@gmail.com>
2018-01-28 15:51:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 cd ~
 sudo apt -y update
- sudo apt -y upgrade
- sudo apt -y install gcc make python2.7 nodejs npm git nodejs-legacy
- git clone git://github.com/c9/core.git c9sdk
- cd c9sdk
- scripts/install-sdk.sh
- cd ~
-  sudo mv c9sdk /opt/
-  mkdir workspace
-   echo " /usr/bin/node /opt/c9sdk/server.js -l 127.0.0.1 -w ~/workspace" > cloud9
-   sudo mv cloud9 /usr/local/bin/
- sudo chmod 755 /usr/local/bin/cloud9
-  echo 'var ws = new ActiveXObject("WScript.Shell");ws.Run("C:\\Windows\\System32\\bash.exe -c \"/usr/local/bin/cloud9 -D\"", 0);' > cloud9.js
+sudo apt -y upgrade
+sudo apt -y install gcc make python2.7 nodejs npm git nodejs-legacy
+git clone git://github.com/c9/core.git c9sdk
+cd c9sdk
+scripts/install-sdk.sh
+cd ~
+sudo mv c9sdk /opt/
+mkdir workspace
+echo " /usr/bin/node /opt/c9sdk/server.js -l 127.0.0.1 -w ~/workspace" > cloud9
+sudo mv cloud9 /usr/local/bin/
+sudo chmod 755 /usr/local/bin/cloud9
+echo 'var ws = new ActiveXObject("WScript.Shell");ws.Run("C:\\Windows\\System32\\bash.exe -c \"/usr/local/bin/cloud9 -D\"", 0);' > cloud9.js

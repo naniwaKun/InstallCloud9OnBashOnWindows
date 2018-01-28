b2e3ee4e3f6d8075b1976d97e125c749285de95b
naniwaKun <radio.naniwa@gmail.com>
2018-01-28 15:51:42 +0900

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,3 +1,1 @@
-lxrun /install /y
-lxrun /setdefaultuser %1
 "C:\Windows\System32\bash.exe" -c  "curl -sf https://raw.githubusercontent.com/naniwaKun/InstallCloud9OnBashOnWindows/master/install.sh | sh -s"
